a20533839399b4c10368865d08a9cac164fa9dd7
DASSOUNDO Adébayo <adebayo.dassoundo@reseau.eseo.fr>
2026-02-01 01:21:48 +0100

    add recomm....

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Liste de tes documents (ajoute autant que tu veux ici)
     const docs = [
         { name: "Recommendation-1_Adébayo_DASSOUNDO", file: "Recommendation-1_Adébayo_DASSOUNDO.pdf" },
-        { name: "Recommendation-2_Adébayo_DASSOUNDO", file: "Recommendation-2_Adébayo_DASSOUNDO.pdf" },
+        { name: "Recommendation-2_Adébayo_DASSOUNDO", file: "Recommendation-2_Adébayo_DASSOUNDO.pdf" }
     ];
 
     // Création du menu
